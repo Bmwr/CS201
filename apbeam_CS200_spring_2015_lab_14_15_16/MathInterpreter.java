9ac3fbfe06b4c9bc1e5b4ca56ed2a5224013b549
Bmwr <andreapbeam@gmail.com>
2015-04-02 09:04:19 -0500

    Organized folders & updated .gitignore.

--- apbeam_CS200_spring_2015_lab_14_15_16/MathInterpreter.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileNotFoundException;
-
-public class MathInterpreter
-{
-  public static void main(String[] args) throws FileNotFoundException
-  {
-    Parser.parse(Tokenizer.token(args[0]));
-  }
-}
